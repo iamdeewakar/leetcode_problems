a589ba40030a84e63fe7c012308b82acacaa02c6
Diwakar Mandal <73119223+iamdeewakar@users.noreply.github.com>
2024-01-17 11:24:48 +0530

    Time: 136 ms (26.06%) | Memory: 73.7 MB (6.92%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         set<int>setform(nums.begin(),nums.end());
-        if(nums.size()==setform.size()){
-            return false;
-        }
-        else{
-            return true;
-        }
-
+        return !(nums.size()==setform.size());
         
     }
 };
