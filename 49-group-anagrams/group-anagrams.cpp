2302810b6557eabbf8da999a3b559aa8657f95b9
Diwakar Mandal <73119223+iamdeewakar@users.noreply.github.com>
2024-01-18 19:04:02 +0530

    Time: 35 ms (42.77%) | Memory: 21.1 MB (64.17%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.empty()){
+            return{{""}};
+        }
+        if(strs.size()==1){
+            return {{strs[0]}};
+        }
+        vector<vector<string>>res;
+        unordered_map<string,vector<string>>MapStore;
+        
+        for(auto str:strs){
+            string indexString = str;
+            sort(indexString.begin(),indexString.end());
+            MapStore[indexString].push_back(str);
+        }
+        for(auto i:MapStore){
+            res.push_back(i.second);
+        }
+        return res;
+        
+    }
+};
