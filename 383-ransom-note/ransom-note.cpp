d3a35bab00bd5b749114227ee9c3ea2c9608af81
Diwakar Mandal <73119223+iamdeewakar@users.noreply.github.com>
2024-02-01 18:20:57 +0530

    Time: 132 ms (5.05%) | Memory: 10 MB (19.00%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -3,18 +3,20 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         sort(ransomNote.begin(),ransomNote.end());
         sort(magazine.begin(),magazine.end());
-        int i = 0 ; int j = 0;
-        while(i < ransomNote.size() && j < magazine.size()){
-            if(ransomNote[i] == magazine[j]){
-                i++;
-                j++;
-            }else{
-                j++;
+        
+
+int count = 0;
+        for(int i=0;i<ransomNote.size();i++){
+            for(int j=0;j<magazine.size();j++){
+                if(ransomNote[i]==magazine[j]){
+                    count++;
+                    magazine.erase(j, 1);
+                    break;
+                }
             }
         }
 
-        return i == ransomNote.size();
-
+        return count == ransomNote.size();
 
     }
 };
