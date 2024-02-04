bbb8629d245e701bb5b41798fadf1696287ef6ed
Diwakar Mandal <73119223+iamdeewakar@users.noreply.github.com>
2024-02-04 14:38:37 +0530

    Time: 5 ms (53.00%) | Memory: 9.6 MB (5.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        string strToLower = "";
         int n = s.length();
-string toLowerStr = "";
-for(int i=0;i<n;i++){
-toLowerStr.push_back(tolower(s[i]));
-}
-string validString = "";
-for(int i=0;i<n;i++){
-    if(isalpha(toLowerStr[i]) || isdigit(toLowerStr[i])){
-            validString.push_back(toLowerStr[i]);
+        for(int i=0;i<n;i++){
+            strToLower.push_back(tolower(s[i]));
         }
-    }
-
-   int start=0,prev=validString.size()-1;
-   while(start<prev){
-       if(validString[start]!=validString[prev]){
-           return false;
-       }
-       start++,prev--;
-   } 
-
-   return true;
-        
+        string validstring ="";
+        for(int i=0;i<n;i++){
+            if(isalpha(strToLower[i]) || isdigit(strToLower[i])){
+                validstring.push_back(strToLower[i]);
+            }
+        }
+        int start =0 , last = validstring.length()-1;
+        while(start < last){
+            if(validstring[start]!=validstring[last]){
+                return false;
+            }
+            start++;
+            last--;
+        }
+        return true;
     }
 };
